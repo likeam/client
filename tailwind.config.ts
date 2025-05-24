0088a46e6727e2b625ef3b27394a2c6db6abe189
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-05-24 20:24:32 +0500

    Initial setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,22 @@
 import type { Config } from "tailwindcss";
+import { createTheme } from "tw-colors";
+import colors from "tailwindcss/colors";
+
+const baseColors = [
+  "gray",
+  "red",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "purple",
+  "pink",
+];
+
+const shadeMapping = {
+  "50": "900",
+  "100": "800",
+};
 
 export default {
   content: [
