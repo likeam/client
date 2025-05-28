e9773eb8716770797eb087515a0e34a2a99a55a2
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-05-28 20:23:45 +0500

    Building Server on backend side

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
-import { setIsSidebarCollapsed } from "@/state";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -11,8 +11,14 @@ const Navbar = () => {
     (state) => state.global.isSidebarCollapsed
   );
 
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   const toggleSidebar = () => {
     dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
   };
 
   return (
@@ -30,7 +36,7 @@ const Navbar = () => {
         <input
           type="search"
           placeholder="Start to search groups & produts"
-          className=" pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+          className=" pl-10 pr-4 py-2 w-40 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
         />
         <div className=" absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
           <Bell className=" text-gray-500 size={20}" />
@@ -38,26 +44,30 @@ const Navbar = () => {
       </div>
       {/* Right Side  */}
       <div className=" flex justify-between items-center gap-5">
-        <div className=" hidden md:flex justify-between items-center gap-5">
+        <div className=" flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}} className="">
-              <Sun className=" cursor-pointer text-gray-500 size={24}" />
+            <button onClick={toggleDarkMode} className="">
+              {isDarkMode ? (
+                <Sun className=" cursor-pointer text-gray-500 size={24}" />
+              ) : (
+                <Moon className=" cursor-pointer text-gray-500 size={24}" />
+              )}
             </button>
           </div>
-          <div className=" relative">
+          <div className=" hidden md:block relative">
             <Bell className=" cursor-pointer text-gray-500 size={24}" />
             <span className=" absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
               4
             </span>
           </div>
-          <hr className=" w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
-          <div className=" flex items-center gap-3 cursor-pointer">
+          <hr className=" hidden md:block w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
+          <div className="flex  items-center gap-3 cursor-pointer">
             <div className=" w-9 h-9">Image</div>
-            <span className=" font-semibold">Hania G</span>
+            <span className="hidden md:block font-semibold">Hania G</span>
           </div>
         </div>
         <Link href="/settings">
-          <Settings className=" cursor-pointer text-gray-500 " size={24} />
+          <Settings className="  cursor-pointer text-gray-500 " size={24} />
         </Link>
       </div>
     </div>
--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,22 +1,59 @@
 "use client";
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/state";
-import { LucideIcon, Menu } from "lucide-react";
+import {
+  Archive,
+  CircleDollarSign,
+  Clipboard,
+  Layout,
+  LucideIcon,
+  Menu,
+  SlidersHorizontal,
+  User,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
-// interface SidebarLinksProps {
-//   href: string;
-//   icon: LucideIcon;
-//   label: string;
-//   isCollapsed: boolean;
-// }
+interface SidebarLinksProps {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+  isCollapsed: boolean;
+}
 
-// const SidebarLink = ({
-//   href,
-//   icon: Icon,
-//   label,
-//   isCollapsed,
-// }: SidebarLinksProps);
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
+}: SidebarLinksProps) => {
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href={href}>
+      <div
+        className={`cursor-pointer flex items-center  ${
+          isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
+        } hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
+          isActive ? "bg-blue-200 text-white" : ""
+        }
+          `}
+      >
+        <Icon className=" w-6 h-6 !text-gray-700" />
+        <span
+          className={`${
+            isCollapsed ? "hidden" : "block"
+          } font-medium text-gray-700`}
+        >
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 const Sidebar = () => {
   const dispatch = useAppDispatch();
@@ -58,9 +95,46 @@ const Sidebar = () => {
       </div>
 
       {/* Links  */}
-      <div className=" flex-grow mt-8">{/* Links here */}</div>
+      <div className=" flex-grow mt-8">
+        <SidebarLink
+          href="/dashboard"
+          icon={Layout}
+          label="Dashboard"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/inventory"
+          icon={Archive}
+          label="Inventory"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/products"
+          icon={Clipboard}
+          label="Products"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/users"
+          icon={User}
+          label="Users"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/settings"
+          icon={SlidersHorizontal}
+          label="Settings"
+          isCollapsed={isSidebarCollapsed}
+        />
+        <SidebarLink
+          href="/expenses"
+          icon={CircleDollarSign}
+          label="Expenses"
+          isCollapsed={isSidebarCollapsed}
+        />
+      </div>
       {/* Footer  */}
-      <div className="">
+      <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
         <p className=" text-center text-xs text-gray-500">&copy; 2025 EStock</p>
       </div>
     </div>
